68abbee087274507fda60789748e3ff1006f9869
rtelep <rick@knotbird.com>
2010-03-02 16:04:10 -0500

    using .couchapprc

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd fmr
-couchapp push . http://rtelep:foobar@localhost:5984/a/
+couchapp push
 cd ../auth
-couchapp push . http://rtelep:foobar@localhost:5984/auth/
+couchapp push
 cd ..
 
